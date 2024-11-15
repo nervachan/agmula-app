3754a8538fff23c4ea57d502fcaf82d1b04c5ead
NervaChan <nervagrf@gmail.com>
2024-11-15 21:41:49 +0800

    commit for labact 5

--- app/src/main/java/com/nervagodz/agmula/MainActivity.kt
+++ app/src/main/java/com/nervagodz/agmula/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.nervagodz.agmula
 
+import android.app.Notification
+import android.app.Notification.Builder
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
 import android.os.Bundle
 import android.view.View
 import android.webkit.WebView
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -13,12 +19,40 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.nervagodz.agmula.ui.theme.AgmulaTheme
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+            val CHANNEL_ID = "ChannelID"
+            val CHANNEL_NAME = "Agmula"
+            val NOTIF_ID =0
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home_page) // start of app
+        Toast.makeText(applicationContext, "Ensure internet is enabled for best experience", Toast.LENGTH_LONG).show()
+
+        var builder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.gears)
+            .setContentTitle("Agmula")
+            .setContentText("Agmula is running, please enjoy the app!")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setAutoCancel(true)
+
+        val youTubePlayerView: YouTubePlayerView = findViewById(R.id.youtube_player_view)
+
+        lifecycle.addObserver(youTubePlayerView)
+
+        youTubePlayerView.addYouTubePlayerListener(object: AbstractYouTubePlayerListener() {
+            override fun onReady(youTubePlayer: YouTubePlayer) {
+                val videoId ="5bhcCpNTbC0"
+                youTubePlayer.loadVideo(videoId, 0f)
+            }
+        })
     }
 
 
@@ -33,15 +67,16 @@ class MainActivity : ComponentActivity() {
             setContentView(R.layout.forecast_page) // button for the forecast
             val webView= findViewById<WebView>(R.id.forecastView)
             webView.loadUrl("https://weather.com/en-PH/weather/today/l/dd6a0ea2120e46fec2a46e541100ae99ffe70fe5edcffe73af67ddf0980042db")
+
+
+
         }
 
         fun seeAvailPlants(view: View){
             setContentView(R.layout.garden_database) // button for the available plants wiki
+            Toast.makeText(applicationContext, "Credits: PlantVillage | plantvillage.psu.edu", Toast.LENGTH_SHORT).show()
         }
 
-        fun seePlantSickness(view: View){
-            setContentView(R.layout.garden_sickness) // button for the possible plant sickness wiki
-        }
 
         fun seeBaguioBeans(view: View){
             setContentView(R.layout.baguio_beans)
@@ -92,6 +127,9 @@ class MainActivity : ComponentActivity() {
             webView.loadUrl("https://plantvillage.psu.edu/topics/strawberry/infos")
         }
 
+        fun showToast(view: View){
+            Toast.makeText(applicationContext, "Hello!", Toast.LENGTH_SHORT).show()
+        }
 
 
 
